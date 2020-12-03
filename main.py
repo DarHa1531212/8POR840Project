9be9c2b6697f5e92db077d6647f3c5d9e3c33d00
Vincent Gagnon <vincentgagnon53@gmail.com>
2020-12-03 09:05:24 -0500

    created filtered data csv file

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pandas as pd
 from _datetime import datetime
 import glob
 
-before = datetime.now()
-
-path = r'data' # use your path
+# Our original data took 19 GB of storage and even more in memory, so we decided to filter it to only keep
+# the players that were the best rank, Diamond. We also only kept the main game mode which is BOMB. That reduces our
+# data size to 83 MB, which is very acceptable.
+# The commented code below is the one used to create our csv file with the filtered data.
+'''
+path = r'data'
 all_files = glob.glob(path + "/*.csv")
 
 li = []
@@ -16,11 +19,8 @@ for filename in all_files:
     li.append(df)
 
 df = pd.concat(li, axis=0, ignore_index=True)
+'''
+# df.to_csv(r"C:\Users\vince\Documents\8POR840Project\data.csv", index=False)
 
-after = datetime.now()
-
+df = pd.read_csv('data.csv')
 print(df.info())
-
-# TODO: write dataframe in csv file
-
-print(after - before)
